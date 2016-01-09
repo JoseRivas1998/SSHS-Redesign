2b82d247f4a2927347a9225758904992c9af2a79
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-01-09 10:31:59 -0800

    Update whatsNew.php

--- whatsNew.php
+++ whatsNew.php
@@ -10,7 +10,7 @@
 				<a href="/resources/content/performing/" class="list-group-item"><i class="fa fa-ticket fa-fw"></i> <span class="list-text">Upcoming Performances &amp; Tickets</span></a>
 				<a href="https://github.com/JoseRivas1998/SSHS-Redesign/commits/master" class="list-group-item" target="_blank"><i class="fa fa-github fa-fw"></i> <span class="list-text">Track Redesign Progress</span></a>
 				<a href="http://goo.gl/forms/upqYx0feXm" class="list-group-item" target="_blank"><i class="fa fa-comment fa-fw"></i> <span class="list-text">Give Feedback on the Redesign</span></a>
-				<a href="https://github.com/JoseRivas1998/SSHS-Redesign/releases/latest" class="list-group-item" target="_blank"><i class="fa fa-tag fa-fw"></i> <span class="list-text">Latest Release</span></a>
+				<a href="https://github.com/Santa-Susana-High-School/SSHS-Redesign/releases/latest" class="list-group-item" target="_blank"><i class="fa fa-tag fa-fw"></i> <span class="list-text">Latest Release</span></a>
 				<a href="resources/content/performing/" id="nextShow"></a>
 			</div>
 		</div>
