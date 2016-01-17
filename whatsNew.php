287b9798b60445ec4962d6b2ee40b2b162fd5efc
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-01-16 22:37:09 -0800

    Replicated What's New From Current Site

--- whatsNew.php
+++ whatsNew.php
@@ -5,14 +5,15 @@
 		</div>
 		<div class="panel-body panel-row-1">
 			<div class="list-group">
+				<a href="https://docs.google.com/forms/d/1d46u-l8Qj-6VBzJMczm3G3-RzgTa90I7sxSivjnhf2U/viewform?c=0&w=1" class="list-group-item" target="_blank"><i class="fa fa-file-text"></i> <span class="list-text">Application Form 2016-2017</span></a>
+				<a href="/resources/downloads/2nd-semester-schedule-issues-for-email.pdf" class="list-group-item" target="_blank"><i class="fa fa-info fa-fw"></i> <span class="list-text">2nd Semester Schedule Info Sheet</span></a>
 				<a href="https://docs.google.com/forms/d/1eUuIg_gW7344jxS7V9yCIcycQYtuiEWqg-YHV5xZz20/viewform" class="list-group-item" target="_blank"><i class="fa fa-envelope fa-fw"></i> <span class="list-text">Mailing List for 2016-2017</span></a>
-				<a class="list-group-item" href="/resources/content/speakerseries/"><i class="fa fa-graduation-cap fa-fw"></i> <span class="list-text">Academic Speaker Series</span></a>
+				<a href="/resources/content/speakerseries/" class="list-group-item"><i class="fa fa-graduation-cap fa-fw"></i> <span class="list-text">Academic Speaker Series</span></a>
 				<a href="/resources/content/performing/" class="list-group-item"><i class="fa fa-ticket fa-fw"></i> <span class="list-text">Upcoming Performances &amp; Tickets</span></a>
-				<a href="https://github.com/Santa-Susana-High-School/SSHS-Redesign/commits/master" class="list-group-item" target="_blank"><i class="fa fa-github fa-fw"></i> <span class="list-text">Track Redesign Progress</span></a>
-				<a href="http://goo.gl/forms/upqYx0feXm" class="list-group-item" target="_blank"><i class="fa fa-comment fa-fw"></i> <span class="list-text">Give Feedback on the Redesign</span></a>
-				<a href="https://github.com/Santa-Susana-High-School/SSHS-Redesign/releases/latest" class="list-group-item" target="_blank"><i class="fa fa-tag fa-fw"></i> <span class="list-text">Latest Release</span></a>
-				<a href="resources/content/performing/" id="nextShow"></a>
+				<a href="http://sshsauditions.weebly.com/" class="list-group-item" target="_blank"><i class="fa fa-font fa-fw"></i> <span class="list-text">Performing Arts Auditions</span></a>
+				<a href="https://youtu.be/FQi4qlOCrmk" class="list-group-item" target="_blank"><i class="fa fa-youtube-play fa-fw"></i> <span class="list-text">Video for Parents: Understanding your child's CAASPP Test Score</span></a>
 			</div>
+			<a href="resources/content/performing/" id="nextShow"></a>
 		</div>
 	</div>
 </div>
