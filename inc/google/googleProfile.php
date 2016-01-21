0ac8e7208cff300a26aa6b907946d9fa44e64675
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-01-21 14:48:12 -0800

    Starting Club Form

---
+++ inc/google/googleProfile.php
@@ -0,0 +1,2 @@
+<div class="col-xs-12" id="googleProfile"></div>
+<div class="col-xs-12"><div class="g-signin2" data-onsuccess="onSignIn"></div></div>
---
+++ js/signoutGoogle.js
@@ -0,0 +1,5 @@
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut();
+    $("#googleProfile").html("");
+}
