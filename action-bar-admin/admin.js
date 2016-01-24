e7500e208de8adf0a7a9fc09a4d4ef59d3fa7c86
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-01-24 15:21:19 -0800

    Generate Edit SQL

--- action-bar-admin/admin.js
+++ action-bar-admin/admin.js
@@ -20,6 +20,7 @@ $("#newForm").submit(function(event) {
       cache: false,
       success: function(data) {
         $("#newFormOut").html(data);
+        loadBarList();
       }
     });
   } else {
@@ -57,4 +58,34 @@ $(document).ready(function() {
 
 $("#editBarList").on('change', function() {
   onListChange();
+});
+
+$("#editForm").submit(function(event) {
+  var auth2 = gapi.auth2.getAuthInstance();
+  if(!(auth2.isSignedIn.get()) || profile == null) {
+    $("#newFormOut").html("<div class='alert alert-danger'>You Must Be Logged in to Google</div>");
+  } else if(profile.getEmail().indexOf("@simivalleyusd.org") > -1) {
+    $("#newFormOut").html("<div class='alert alert-warning'><i class='fa fa-spinner fa-spin'></i> Sending Data To Server</div>");
+    var form = $("#newForm").children();
+    var publishDate = formatDateTimeLocal(form.find("input[name='publish']").val());
+    var showUntilDate = formatDateTimeLocal(form.find("input[name='showUntil']").val());
+    $.ajax({
+      type: "POST",
+      url: "editActionBar.php",
+      data: {
+        "barId": $("#editBarList").val(),
+        "barText": form.find("input[name='barText']").val(),
+        "barLink": form.find("input[name='barLink']").val(),
+        "publishDate": publishDate,
+        "showUntilDate": showUntilDate,
+        "userEmail": profile.getEmail().substring(0 , profile.getEmail().indexOf("@"))
+      },
+      cache: false,
+      success: function(data) {
+        $("#newFormOut").html(data);
+      }
+    });
+  } else {
+    $("#newFormOut").html("<div class='alert alert-danger'>Your Email is not a Simi Valley USD account.</div>")
+  }
 });
---
+++ action-bar-admin/editActionBar.php
@@ -0,0 +1,67 @@
+<?php
+
+$root = $_SERVER["DOCUMENT_ROOT"];
+
+include($root . "/inc/dbConnect.php");
+
+$output;
+
+$userEmail = $_POST["userEmail"];
+$userId = -1;
+
+$getIdSql = "SELECT * FROM users WHERE email = '$userEmail' LIMIT 1";
+
+$usersResult = mysqli_query($conn, $getIdSql);
+if(mysqli_num_rows($usersResult) > 0) {
+  while($usersRow = mysqli_fetch_assoc($usersResult)) {
+    $userId = $usersRow["id"];
+  }
+}
+
+if($userId != -1) {
+  $getPermissionsSql = "SELECT * FROM userPermisions WHERE userId = $userId AND (tableAccess = 'actionBar' OR tableAccess = 'full')";
+  $permissionResult = mysqli_query($conn, $getPermissionsSql);
+  $fullEmail = "$userEmail@simivalleyusd.org";
+  if(mysqli_num_rows($permissionResult) > 0) {
+    $barId = $_POST["barId"];
+    $barText = $_POST["barText"];
+    $barLink = $_POST["barLink"];
+    $publishDate = $_POST["publishDate"];
+    $showUntilDate = $_POST["showUntilDate"];
+    if($barText) {
+      if($publishDate) {
+        if($showUntilDate) {
+          $editSql = "UPDATE actionBar SET
+          dateUpdated = NOW(),
+          userUpdated = '$fullEmail',
+          barText = '$barText',
+          publishDate = '$publishDate',
+          showUntil = '$showUntilDate'";
+          if(empty($barLink)) {
+            $editSql .= ", link = NULL";
+          } else {
+            $editSql .= ", link = '$barLink'";
+          }
+          $editSql .= " WHERE id = $barId";
+          $output = "<div class='alert alert-success'>$editSql</div>";
+        } else {
+          $output = "<div class='alert alert-danger'>Enter Show Until Date</div>";
+        }
+      } else {
+        $output = "<div class='alert alert-danger'>Enter Publish Date</div>";
+      }
+    } else {
+      $output = "<div class='alert alert-danger'>Enter Bar Text</div>";
+    }
+  } else {
+    $output = "<div class='alert alert-danger'>Sorry, your email does not have permission to manage this page.</div>";
+  }
+} else {
+  $output = "<div class='alert alert-danger'>Sorry, your email does not have permission to manage the website.</div>";
+}
+
+echo $output;
+
+mysqli_close($conn);
+
+?>
