71ad26df32907bc1cb0b1ac1de6a8e3cbf76cc2f
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-02-01 09:42:27 -0800

    Using http_host

--- callback/index.php
+++ callback/index.php
@@ -16,7 +16,7 @@ $client->setApplicationName("SSHS - Admin Login");
 $client->setClientId(getenv("GOOGLE_CLIENT_ID"));
 $client->setClientSecret(getenv("GOOGLE_CLIENT_SECRET"));
 $client->addScope(Google_Service_Oauth2::USERINFO_EMAIL);
-$client->setRedirectUri('http://localhost/callback');
+$client->setRedirectUri('http://'. $_SERVER["HTTP_HOST"] . '/callback');
 
 $service = new Google_Service_Oauth2($client);
 
