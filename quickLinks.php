5f625a357ce28abb0f01b84e8627262e5ac08ea4
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-02-05 14:58:37 -0800

    Added Playlist to Home Page

--- quickLinks.php
+++ quickLinks.php
@@ -4,7 +4,10 @@
             <div class="panel-title"><i class="fa fa-link"></i> Quick Links</div>
         </div>
         <div class="panel-body panel-row-1" id="quick">
-            <div class="list-group">
+          <div class="embed-responsive embed-responsive-16by9" id="pl">
+              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/videoseries?list=PLS4YhFEqQEh1eXEhIKbY8Kyt0a64B5Tv6" frameborder="0" allowfullscreen></iframe>
+          </div>
+            <div class="list-group" id="quickList">
               <?php
                 include ("getHomeLinks.php");
                 getHomeLinks('quick');
