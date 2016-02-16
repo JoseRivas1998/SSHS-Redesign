e90602d1603d2c7cf6f6d2b2edf953529568605e
Aidan <achisum@hotmail.com>
2016-02-16 14:51:37 -0800

    made pages for individual languages. made css file

--- language-classes/german.php
+++ language-classes/german.php
@@ -39,9 +39,8 @@
 								<!--first pic-->
 								<img src="" class="img-responsive">
 							</div>
-							<div class="col-xs-12 col-sm-6" id="box";>
-								<!--first info-->
-								test
+							<div class="col-xs-12 col-sm-6" id="box">
+								What is German class like at Santa Susana High School?  First of all, Frau Newell is a native of Germany.  She was born and raised in Ingolstadt, Bavaria.  Her entire family still lives there!  As a result, she has a deep understanding and knowledge of the language and culture.  She loves to incorporate as many cultural facts and traditions as possible in addition to regular grammar and vocabulary studies.  Some of the highlights include celebrating German Week in October; building lanterns for the Sankt Martin's celebration in November; experiencing German Christmas traditions in December; going on a field trip in January; celebrating Fasching and playing German Easter Games in the spring; and playing soccer against other German classes before the end of the semester.  Let's not forget the possibility to host German exchange students and then return the visit over the summer!  
 							</div>
 							
 						</div>
