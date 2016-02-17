085d060079cada9abbb95200972ac50ed3241d9a
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-02-16 22:19:54 -0800

    New Home Page

---
+++ showMastersKssh.php
@@ -0,0 +1,27 @@
+<div class="col-xs-12 col-md-4">
+  <div class="col-xs-12">
+    <a href="resources/content/performing/" id="nextShow"></a>
+  </div>
+  <div class="col-xs-12">
+    <a href="webmasters/">
+      <div class="row homeLink">
+        <div class="col-xs-4">
+          <i class="fa fa-code fa-4x"></i>
+        </div>
+        <div class="col-xs-8">
+          <strong>Webmasters</strong>
+        </div>
+      </div>
+    </a>
+  </div>
+  <div class="col-xs-12">
+    <ul class="nav nav-pills nav-justified">
+      <li class="active" role="presentation">
+        <a href="video"><i class="fa fa-television"></i> KSSH News At Noon</a>
+      </li>
+    </ul>
+    <div class="embed-responsive embed-responsive-16by9" id="pl">
+        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/videoseries?list=PLS4YhFEqQEh1eXEhIKbY8Kyt0a64B5Tv6" frameborder="0" allowfullscreen></iframe>
+    </div>
+  </div>
+</div>
--- whatsNew.php
+++ whatsNew.php
@@ -1,15 +1,13 @@
-<div class="col-xs-12 col-sm-4">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <div class="panel-title"><i class="fa fa-newspaper-o"></i> What's New</div>
-        </div>
-        <div class="panel-body panel-row-1">
-            <div class="list-group">
-              <?php
-                getHomeLinks('whatsNew');
-              ?>
-            </div>
-            <a href="resources/content/performing/" id="nextShow"></a>
-        </div>
+<div class="col-xs-12">
+  <div class="row homeLink">
+    <div class="col-xs-5">
+      <i class="fa fa-newspaper-o fa-5x"></i>
     </div>
+    <div class="col-xs-7 homeLinkTitle">
+      What's New
+    </div>
+  </div>
 </div>
+<?php
+  getNewHomeLinks("whatsNew");
+?>
