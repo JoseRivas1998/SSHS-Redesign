8feb3ec2bfb0881c1f159c6bf4f6bc696720cc4b
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-02-17 20:09:14 -0800

    About Us Icon Fixed Width

--- aboutUs.php
+++ aboutUs.php
@@ -1,7 +1,7 @@
 <div class="col-xs-12">
   <div class="row homeLink">
     <div class="col-xs-5">
-      <i class="fa fa-info fa-5x"></i>
+      <i class="fa fa-info fa-5x fa-fw"></i>
     </div>
     <div class="col-xs-7 homeLinkTitle">
       About Us
