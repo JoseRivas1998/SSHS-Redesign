198e940fd3d51ce041a7d1ba62c717e61ab1077d
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-02-26 14:47:42 -0800

    iPhone fix 4

--- calendar.php
+++ calendar.php
@@ -1,3 +1,3 @@
-<div class="col-xs-12 col-sm-6 col-md-4">
+<div class="col-xs-12 col-sm-6 col-md-4" id="calendarDiv">
   <iframe id="calendarFrame" src="https://www.google.com/calendar/embed?title=Calendar&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=310&amp;wkst=1&amp;bgcolor=%23dddddd&amp;src=simivalleyusd.org_36guu8uoe6eqt49dm7jegglc9k@group.calendar.google.com&amp;color=%231B887A&amp;ctz=America%2FLos_Angeles" width="100%"></iframe>
 </div>
