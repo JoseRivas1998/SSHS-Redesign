c0e6acf3768aeeafccc2f3f5ea5fcf19b85356c2
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-03-11 12:49:43 -0800

    #248 Sections can be toggled

--- aboutUs.php
+++ aboutUs.php
@@ -1,13 +1,18 @@
 <div class="col-xs-12">
-  <div class="row homeLink">
-    <div class="col-xs-5">
-      <i class="fa fa-info fa-5x fa-fw"></i>
+  <a href="javascript:void(0)" onclick="toggleId('#aboutUs', '#aboutUsIcon');">
+    <div class="row homeLink">
+      <div class="col-xs-5">
+        <i class="fa fa-info fa-5x fa-fw"></i>
+      </div>
+      <div class="col-xs-7 homeLinkTitle">
+        <i class="fa fa-toggle-up" id="aboutUsIcon"></i>
+        About Us
+      </div>
     </div>
-    <div class="col-xs-7 homeLinkTitle">
-      About Us
-    </div>
-  </div>
+  </a>
 </div>
-<?php
-  getNewHomeLinks("aboutUs");
-?>
+<div id="aboutUs">
+  <?php
+    getNewHomeLinks("aboutUs");
+  ?>
+</div>
