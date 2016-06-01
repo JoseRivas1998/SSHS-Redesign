fface8b3c755c32925ec8b4cdc3a351a9e04d087
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-06-01 08:54:02 -0700

    #310 AP English and APUSH

---
+++ students/apsummer/apush.php
@@ -0,0 +1,12 @@
+<div class="col xs-12 col-sm-6">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <div class="panel-title">Mrs. Wolloch - AP US History</div>
+    </div>
+    <div class="panel-body">
+      <div class="list-group">
+        <a href="apush.html" onclick="return popup(this, 'AP US History Summer')"class="list-group-item">Summer Assignment</a>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ students/apsummer/eng12.php
@@ -0,0 +1,12 @@
+<div class="col xs-12 col-sm-6">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <div class="panel-title">Mrs. Bradley &amp; Mr. White - AP English 12</div>
+    </div>
+    <div class="panel-body">
+      <div class="list-group">
+        <a href="http://virtualclassroomsshs.weebly.com/ancient-lit-summer-reading.html" class="list-group-item">Ancient Lit (Summer Reading)</a>
+      </div>
+    </div>
+  </div>
+</div>
