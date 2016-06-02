6a47e540f4c7f607c6f4d132da42429bef8566a5
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-06-02 10:43:02 -0700

    #310 Mr. Thaler - AP Chemistry

---
+++ students/apsummer/apchem.php
@@ -0,0 +1,12 @@
+<div class="col-xs-12 col-sm-6">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <div class="panel-title">Mr. Thaler - AP Chemistry</div>
+    </div>
+    <div class="panel-body">
+      <div class="list-group">
+        <a href="http://www.mrthaler.net/uploads/6/5/4/3/65431157/ap_chem_sum_assignmt_16-17.pdf" class="list-group-item">Summer Assignment</a>
+      </div>
+    </div>
+  </div>
+</div>
