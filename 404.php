06f10ba505f25dcf645096090a821aaecdf2a6c8
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-07-08 15:21:42 -0700

    #329 Read Pages in 404 Page

--- 404.php
+++ 404.php
@@ -1,3 +1,47 @@
+<?php
+$root = $_SERVER["DOCUMENT_ROOT"];
+
+include($root . "/inc/dbConnect.php");
+
+$responseCode = "404 Not Found";
+$title = "404 Not Found";
+$content = "<p>Unfortunately, the page <a id=\"link\"></a> could not be found, please check the url and try again.</p><button class=\"btn btn-default\" onclick=\"history.back(-1);\"><i class=\"fa fa-arrow-left\"></i> Return to Previous Page</button>";
+$sidenav = false;
+
+$categories = array();
+$categories['students'] = "$root/students/left-nav.php";
+$categories['parents'] = "$root/parents/left-nav.php";
+$categories['staff'] = "$root/staff/left-nav.php";
+$categories['community'] = "$root/community/left-nav.php";
+
+$requestURI = $_SERVER["REQUEST_URI"];
+
+$sql = "SELECT * FROM fullPages WHERE `path` = '$requestURI' LIMIT 1";
+
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result) > 0) {
+  while($row = mysqli_fetch_assoc($result)) {
+    $responseCode = "200 OK";
+    $title = $row['title'];
+    $content = $row["content"];
+    $sidenav = is_null($row['sidenav']) !== true;
+    if($sidenav) {
+      $category = $row['sidenav'];
+    }
+  }
+}
+
+$sapi_type = php_sapi_name();
+if (substr($sapi_type, 0, 3) == 'cgi') {
+    header("Status: $responseCode");
+  } else {
+    header("HTTP/1.1 $responseCode");
+  }
+
+mysqli_close($conn);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,7 +51,7 @@
     $path .= "/inc/head.php";
     include($path);
     ?>
-    <title>Santa Susana High School</title>
+    <title><?php echo $title ?> | Santa Susana High School</title>
     <style>
         #link:hover {
             cursor: default;
@@ -31,15 +75,17 @@
 <section id="content-wrapper">
     <div class="container" id="content-container">
         <div class="row">
-            <div class="col-xs-12">
-                <div class="section-title">404 Not Found</div>
-                <p>
-                    Unfortunately, the page <a id="link"></a> could not be found, please check the url and try again.
-                </p>
-                <button class="btn btn-default" onclick="history.back(-1);"><i class="fa fa-arrow-left"></i> Return to
-                    Previous Page
-                </button>
-            </div>
+          <?php
+            if($sidenav) {
+              include($categories[$category]);
+              echo "<div class=\"col-xs-12 col-sm-9\">";
+            } else {
+              echo "<div class=\"col-xs-12\">";
+            }
+            echo "<div class=\"section-title\">$title</div>";
+            echo "$content";
+            echo "</div>";
+          ?>
         </div>
     </div>
 </section>
--- staff/index.php
+++ staff/index.php
@@ -31,21 +31,7 @@
 <section id="content-wrapper">
     <div class="container" id="content-container">
         <div class="row">
-            <div class="col-xs-12 col-sm-3 side-nav">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <div class="panel-title">Staff</div>
-                    </div>
-                    <div class="panel-body">
-                        <div class="list-group">
-                          <?php
-                            include_once($_SERVER['DOCUMENT_ROOT'] . '/inc/sideNav.php');
-                            loadSideNav('staff');
-                          ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php include('left-nav.php') ?>
             <div class="col-xs-12 col-sm-9">
                 <div class="section-title">General Info</div>
                 <div class="row" id="staffPicRow">
